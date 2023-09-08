61243a54746d02a0996f0fc69334d2c8a7206136
Sergey <sergey.ryzhov2001@yandex.ru>
2023-09-08 17:53:06 +0300

    Добавил функцию remove_stopwords Хз работает или нет

--- main.py
+++ main.py
@@ -3,9 +3,35 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+def remove_stopwords(text):
+    # Разделяем текст на слова
+    words = word_tokenize(text)
+
+    # Загружаем список стоп-слов на английском языке
+    stop_words = set(stopwords.words('english'))
+
+    # Удаляем стоп-слова из списка слов
+    filtered_words = [word for word in words if word.lower() not in stop_words]
+
+    # Объединяем отфильтрованные слова обратно в текст
+    filtered_text = ' '.join(filtered_words)
+
+    # Возвращаем результат
+    return filtered_text
 
 # Загрузка данных
 data = pd.read_excel('CRA_train_1200.xlsx')
+
+# Применяем функцию remove_stopwords к столбцу pr_txt
+data['pr_txt'] = data['pr_txt'].apply(remove_stopwords)
+
+# Сохраняем изменения в датасете
+data.to_csv('processed_dataset.csv', index=False)
+
+
 
 # Предобработка данных
 # ...
